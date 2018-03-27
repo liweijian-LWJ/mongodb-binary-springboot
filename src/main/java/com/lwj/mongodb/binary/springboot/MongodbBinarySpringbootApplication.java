dc45a753ffc039c73a705eb396645c41b4e986c6
liweijian <liweijian@hdsc.com>
2018-03-27 18:23:09 +0800

    Initial commit

---
+++ src/main/java/com/lwj/mongodb/binary/springboot/MongodbBinarySpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.lwj.mongodb.binary.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbBinarySpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbBinarySpringbootApplication.class, args);
+	}
+}
