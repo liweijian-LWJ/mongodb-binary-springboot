dbc63ea1db9058173b16a22e52d70e0a802b1090
liweijian <liweijian@hdsc.com>
2018-03-27 18:38:17 +0800

    首次提交

---
+++ src/main/java/com/lwj/mongodb/binary/springboot/controller/BinaryController.java
@@ -0,0 +1,11 @@
+package com.lwj.mongodb.binary.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+
+/*
+*@Author:liweijian
+*/
+@Controller
+public class BinaryController {
+
+}
